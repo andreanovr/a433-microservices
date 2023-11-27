bc68dda57fe9a5f8850bd1d17a884b98e3014fa8
Andreanov Ridhovan <andreanovr@gmail.com>
2023-11-28 03:20:52 +0700

    working on submission

---
+++ build_push_image.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Perintah untuk membuat Docker image dari Dockerfile yang tadi dibuat, dengan nama image item-app, dan memiliki tag v1.
+docker build -t item-app:v1 .
+
+# Melihat daftar image di lokal.
+docker images
+
+# Mengubah nama image agar sesuai dengan format Docker Hub (atau GitHub Packages bila menerapkan saran keempat).
+docker tag item-app:v1 ghcr.io/andreanovr/item-app:v1
+
+# Login ke Docker Hub (atau GitHub Packages bila menerapkan saran keempat) via Terminal.
+echo $GHP_TOKEN | docker login ghcr.io -u andreanovr --password-stdin
+
+# Mengunggah image ke Docker Hub (atau GitHub Packages bila menerapkan saran keempat).
+docker push ghcr.io/andreanovr/item-app:v1
